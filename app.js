9634b662f26468e57536e326d734f5be69e8cb62
Jason Cheng <qishen.cheng@ivisgroup.com>
2014-05-09 17:26:16 +0800

    Install NPM Package express-session, Create Layout.ejs And Index.ejs,  Require mongoose, Setup Passport

--- app.js
+++ app.js
@@ -4,17 +4,18 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
-// Require Passport And Strategies
+// Setup Dependencies For Passport Auth Tut
 var passport = require('passport');
+var flash    = require('connect-flash');
+var mongoose = require('mongoose');
 
-// Require Connect Flash
-var flash = require('connect-flash');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,11 +28,17 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Setup Middlewares For Passport Auth
+app.use(session({ secret: 'iamcheng' }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
 app.use('/', routes);
 app.use('/users', users);
 
 /// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -42,7 +49,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -53,13 +60,12 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
         error: {}
     });
 });
-
 
 module.exports = app;
